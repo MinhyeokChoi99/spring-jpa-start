fe7cefaa4605a4aff235866d16bfd9f8805e0526
Minhyeok Choi <bbbim5656@gmail.com>
2026-06-14 23:31:45 +0900

    entity gen

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -3,15 +3,20 @@ package jpabook.jpashop;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Transactional
+//    @Rollback(false)
     void testMember() throws Exception {
         //given
         Member member = new Member();
@@ -21,6 +26,8 @@ class MemberRepositoryTest {
         //then
         Member findMember = memberRepository.find(saveId);
         Assertions.assertThat(findMember).isNotNull();
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 
     }
 
